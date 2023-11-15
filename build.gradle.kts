2d45a24cdc9bf3c288804c11e503f2c99b4a1a4b
dom <davide.domini2@unibo.it>
2023-11-15 09:57:24 +0100

    build: update build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,38 @@
 plugins {
     id("java")
+    id("scala")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+scala {
+    zincVersion.set("1.6.1")
+}
+
+sourceSets {
+    main {
+        scala {
+            setSrcDirs(listOf("src/main/scala"))
+        }
+    }
+    test {
+        scala {
+            setSrcDirs(listOf("src/test/scala"))
+        }
+    }
+}
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
 }
 
 dependencies {
+
+    //implementation("dev.jeka:kotlin-plugin:master-SNAPSHOT")
+    implementation("org.scala-lang:scala3-library_3:3.3.1")
+    implementation("dev.storch:core:0.0-05078a8-SNAPSHOT")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
