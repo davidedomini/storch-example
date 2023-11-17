d187eb468ecad0a8969cd3ce9491f95aa9e2ab85
dom <davide.domini2@unibo.it>
2023-11-17 10:20:12 +0100

    fix(BasicExample): remove cuda instructions

--- src/main/scala/it/unibo/BasicExample.scala
+++ src/main/scala/it/unibo/BasicExample.scala
@@ -1,20 +1,15 @@
 package it.unibo
 
-
-import torch.Device.{CPU, CUDA}
-
 object BasicExample extends App {
 
-  private val device = if torch.cuda.isAvailable then CUDA else CPU
-  // device: Device = Device(device = CUDA, index = -1)
-  println(torch.rand(Seq(3, 3), device = device))
+  println(torch.rand(Seq(3, 3)))
   // res1: Tensor[Float32] = dtype=float32, shape=[3, 3], device=CUDA
   // [[0.3990, 0.5167, 0.0249],
   //  [0.9401, 0.9459, 0.7967],
   //  [0.4150, 0.8203, 0.2290]]
 
   // Use device index if you have multiple GPUs
-  println(torch.rand(Seq(3, 3), device = torch.Device(torch.DeviceType.CUDA, 0: Byte)))
+  println(torch.rand(Seq(3, 3)))
   // res2: Tensor[Float32] = dtype=float32, shape=[3, 3], device=CUDA
   // [[0.9722, 0.7910, 0.4690],
   //  [0.3300, 0.3345, 0.3783],
