9c31ca1fce083507e63cd7103d032a443f4e9399
dom <davide.domini2@unibo.it>
2023-11-22 11:11:48 +0100

    feat: add train on iris

--- src/main/scala/it/unibo/iris/CustomNetwork.scala
+++ src/main/scala/it/unibo/iris/CustomNetwork.scala
@@ -1,3 +1,50 @@
-package it.unibo.iris class CustomNetwork {
+package it.unibo.iris
 
+import org.bytedeco.javacpp.PointerScope
+import org.bytedeco.pytorch.OutputArchive
+import torch.*
+
+import scala.util.Using
+
+class CustomNetwork extends nn.Module {
+  val flatten = nn.Flatten()
+  val linearReluStack = register(nn.Sequential(
+    nn.Linear(4, 16),
+    nn.ReLU(),
+    nn.Linear(16, 16),
+    nn.ReLU(),
+    nn.Linear(16, 3)
+  ))
+
+  def apply(x: Tensor[Float32]) =
+    val flattened = flatten(x)
+    val logits = linearReluStack(flattened)
+    nn.functional.softmax(logits, 1)()
+}
+
+object Train extends App {
+  private val model = CustomNetwork()
+  private val dataset = LoadDataset.load("src/main/resources/Iris.csv")
+
+  private val lossFunction = torch.nn.loss.CrossEntropyLoss()
+  private val optimizer = torch.optim.Adam(model.parameters, lr=1e-3, amsgrad = true)
+
+  for (epoch <- 1 to 5) {
+    for (batch <- dataset.batches(50)) {
+      Using.resource(new PointerScope()) { p =>
+        val (f, t) = batch.unzip
+        val feature = torch.stack(f.map(Tensor(_).to(torch.float32)))
+        val target = torch.stack(t.map(Tensor(_).to(torch.float32)))
+        optimizer.zeroGrad()
+        val prediction = model(feature)
+        val loss = lossFunction(prediction, target)
+        loss.backward()
+        optimizer.step()
+      }
+    }
+  }
+
+  private val archive = new OutputArchive()
+  model.save(archive)
+  archive.save_to("src/main/resources/net.pt")
 }
--- src/main/scala/it/unibo/iris/LoadDataset.scala
+++ src/main/scala/it/unibo/iris/LoadDataset.scala
@@ -1,3 +1,43 @@
-package it.unibo.iris class LoadDataset {
+package it.unibo.iris
 
+import scala.util.Random
+
+class Dataset {
+
+  private var data: Seq[(Seq[Double], Seq[Double])] = Seq.empty
+
+  def add(features: Seq[Double], target: String): Unit =
+    data = data :+ (features, oneHotEncode(target))
+
+  def batches(size: Int): Seq[Seq[(Seq[Double], Seq[Double])]] =
+    Random(seed = 42).shuffle(data).grouped(size).toSeq
+
+  private def oneHotEncode(target: String): Seq[Double] = target match {
+      case "Iris-setosa" => Seq(1, 0, 0)
+      case "Iris-versicolor" => Seq(0, 1, 0)
+      case "Iris-virginica" => Seq(0, 0, 1)
+    }
+
+}
+
+object LoadDataset {
+
+  def load(path: String): Dataset =
+    val dataset = Dataset()
+    val buffer = io.Source.fromFile(path)
+    for (line <- buffer.getLines()) {
+      val col: List[String] = line.split(",").map(_.trim).toList
+      val feature: Seq[Double] = col.slice(1, col.length-1).map(_.toDouble)
+      val target = col.last
+      dataset.add(feature, target)
+    }
+    buffer.close()
+    dataset
+}
+
+object TestCsvLoad extends App {
+  private val dataset = LoadDataset.load("src/main/resources/Iris.csv")
+  for (batch <- dataset.batches(50)) {
+    println(batch)
+  }
 }
